407200b79dc76116b6cee2b6eecc30e5758cc597
Ava <reacthmd34@gmail.com>
2023-10-06 16:12:45 +0200

    handle error && install redux

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,6 +2,9 @@ import React from "react";
 import Wrapper from "../assets/wrappers/RegisterPage";
 import { useState } from "react";
 import { Logo, FormRow } from "../components";
+import { toast } from "react-toastify";
+import { useSelector, useDispatch } from "react-redux";
+import { loginUser, registerUser } from "../features/user/userSlice";
 
 const initialState = {
   name: "",
@@ -12,14 +15,27 @@ const initialState = {
 
 const Register = () => {
   const [values, setValues] = useState(initialState);
-
+  const { user, isLoading } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
   const handleChange = (e) => {
-    console.log(e.target);
+    const value = e.target.value;
+    const name = e.target.name;
+    console.log(e.target.value);
+    setValues({ ...values, [name]: value });
   };
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log(e.target);
+    const { email, name, isMember, password } = values;
+    if (!email || !password || (!isMember && !name)) {
+      toast.error("please fill out fields");
+      return;
+    }
+    if (isMember) {
+      dispatch(loginUser({ email: email, password: password }));
+      return;
+    }
+    dispatch(registerUser({ name, email, password }));
   };
 
   const toggleMember = () => {

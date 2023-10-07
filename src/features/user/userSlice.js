6db2b95424256aa7e7ebdb635fa136003f37c2a3
Ava <reacthmd34@gmail.com>
2023-10-07 15:33:13 +0200

    add extraReducers

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -10,25 +10,43 @@ const initialState = {
 export const registerUser = createAsyncThunk(
   "user/registerUser",
   async (user, thunkAPI) => {
-    console.log(`Register User: ${JSON.stringify(user)}`);
+    try {
+      const resp = await customFetch.post("/auth/register", user);
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data.msg);
+    }
   }
 );
 
 export const loginUser = createAsyncThunk(
   "user/loginUser",
   async (user, thunkAPI) => {
-    try {
-      const resp = await customFetch.post("/auth/testingRegister", user);
-      console.log(resp);
-    } catch (error) {
-      console.log(error.response);
-    }
+    console.log(`Logon User: ${JSON.stringify(user)}`);
   }
 );
 
 const userSlice = createSlice({
   name: "user",
   initialState,
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(registerUser.fulfilled, (state, { payload }) => {
+        const { user } = payload;
+        state.isLoading = false;
+        state.user = user;
+
+        toast.success(`Hello There ${user.name}`);
+      })
+      .addCase(registerUser.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        toast.error(payload);
+      });
+  },
 });
 
 export default userSlice.reducer;

1b74db078930bfec0a962230aa81790a41b61343
Ava <reacthmd34@gmail.com>
2023-10-09 15:44:59 +0200

    setup smallSidebar & bigSidebar

--- src/components/BigSidebar.js
+++ src/components/BigSidebar.js
@@ -1,10 +1,28 @@
+import { useSelector } from "react-redux";
 import Wrapper from "../assets/wrappers/BigSidebar";
-const Bigsidebar = () => {
+import { Logo } from "./Logo";
+import NavLinks from "./NavLinks";
+
+const BigSidebar = () => {
+  const { isSidebarOpen } = useSelector((store) => store.user);
   return (
     <Wrapper>
-      <h1>Bigsidebar</h1>
+      <div
+        className={
+          isSidebarOpen
+            ? "sidebar-container "
+            : "sidebar-container show-sidebar"
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
     </Wrapper>
   );
 };
 
-export default Bigsidebar;
+export default BigSidebar;
--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -1,11 +1,11 @@
 import Wrapper from "../assets/wrappers/SmallSidebar";
 import { FaTimes } from "react-icons/fa";
-import { NavLink } from "react-router-dom";
 import { Logo } from "./Logo";
 import { useSelector, useDispatch } from "react-redux";
 import { toggleSidebar } from "../features/user/userSlice";
+import NavLinks from "./NavLinks";
 
-export const SmallSidebar = () => {
+const SmallSidebar = () => {
   const dispatch = useDispatch();
   const { isSidebarOpen } = useSelector((store) => store.user);
 
@@ -28,7 +28,7 @@ export const SmallSidebar = () => {
           <header>
             <Logo />
           </header>
-          <div className="nav-links">nav links</div>
+          <NavLinks toggleSidebar={toggle} />
         </div>
       </div>
     </Wrapper>

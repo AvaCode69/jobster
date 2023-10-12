2d18fb835e2e69e9ed7f386a1845065e01d88a6a
Ava <reacthmd34@gmail.com>
2023-10-12 16:25:35 +0200

    setup addJob

---
+++ src/components/FormRowSelect.js
@@ -0,0 +1,26 @@
+const FormRowSelect = ({ lableText, name, value, handleChange, list }) => {
+  return (
+    <div className="form-row">
+      <label htmlFor={name} className="form-label">
+        {lableText || name}
+      </label>
+      <select
+        name={name}
+        id={name}
+        value={value}
+        onChange={handleChange}
+        className="form-select"
+      >
+        {list.map((itemValue, index) => {
+          return (
+            <option key={index} value={itemValue}>
+              {itemValue}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+};
+
+export default FormRowSelect;
